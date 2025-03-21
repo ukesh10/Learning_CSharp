526fba0b1dd46729920bd02d98da44dbac1ebffc
Ukesh <ukeshsharma10@gmail.com>
2025-03-22 00:37:33 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
